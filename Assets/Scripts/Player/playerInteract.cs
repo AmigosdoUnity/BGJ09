f19ca41a7144de58521e0d6715017ed9f5c6f72f
Vitor <vmenezes@inf.ufsm.br>
2015-06-12 21:53:38 -0300

    Store

---
+++ Assets/Scripts/Player/playerInteract.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerInteract : MonoBehaviour {
+
+	public GameObject store;
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown(KeyCode.P))
+		{
+			store.SetActive(!store.activeSelf);
+		}
+	}
+}

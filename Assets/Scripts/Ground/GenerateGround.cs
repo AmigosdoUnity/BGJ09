83c1269ca05941537373eb41d0bb08d69068eb7b
Vitor <vmenezes@inf.ufsm.br>
2015-06-12 22:23:01 -0300

    fix store2

--- Assets/Scripts/Ground/GenerateGround.cs
+++ Assets/Scripts/Ground/GenerateGround.cs
@@ -3,13 +3,11 @@ using System.Collections;
 
 public class GenerateGround : MonoBehaviour {
 
+	public GameObject lSquare;
+
 	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
+	void Start () 
+	{
 	
 	}
 }

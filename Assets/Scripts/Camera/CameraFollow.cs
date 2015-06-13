782503b537fd55a853a0746e66ce75120c5a1145
Vitor <vmenezes@inf.ufsm.br>
2015-06-12 21:02:00 -0300

    Starting phase

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform target;
+
+
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 temp = new Vector3 (target.position.x, target.position.y, 0);
+		temp.z = transform.position.z;
+
+
+		transform.position = temp;
+	}
+}
---
+++ Assets/Scripts/Player/playerMovement.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerMovement : MonoBehaviour {
+
+	public byte speed;
+
+	public byte jumpSpeed;
+
+	private Rigidbody2D rb;
+
+	private RaycastHit2D hit;
+
+	void Start()
+	{
+		rb = gameObject.GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 vel = rb.velocity;
+
+		vel.x = Input.GetAxis ("Horizontal") * speed * Time.deltaTime * 20;
+
+		hit = Physics2D.Raycast (transform.position, Vector3.down, transform.localScale.y / 2 + 0.1f, 1 << 8);
+
+		if (Input.GetAxisRaw("Vertical") > 0)
+		{
+			if (hit.collider != null)
+			{
+				vel.y = jumpSpeed * Time.deltaTime;
+			}
+		}
+
+
+		rb.velocity = vel;
+	}
+}

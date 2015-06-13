7fcd9bc58637c3fe47eef3a5343147b300effa0c
Vitor <vmenezes@inf.ufsm.br>
2015-06-13 03:23:40 -0300

    DirtGenerator

--- Assets/Scripts/Ground/Espaço.cs
+++ Assets/Scripts/Ground/Espaço.cs
@@ -1,9 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Espaço : MonoBehaviour {
+public class Espaço {
 
 	public Vector3 position;
 
 	public int alt, largura;
+
+	public Espaço (Vector3 _p, int _a, int _l)
+	{
+		position = _p;
+		alt = _a;
+		largura = _l;
+	}
 }
--- Assets/Scripts/Ground/GenerateGround.cs
+++ Assets/Scripts/Ground/GenerateGround.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 public class GenerateGround : MonoBehaviour {
 
 	public GameObject dirt;
-
+	
 	public List<Espaço> e = new List<Espaço>();
 
 	private SpriteRenderer sr;
@@ -18,21 +18,61 @@ public class GenerateGround : MonoBehaviour {
 
 	private void generateGround()
 	{
-		// Cobertura
+		int randL = 0;
+		int randA = 0;
+		int randP = 0;
+
+		int tL = 0;
+		int tA = 0;
+
+		// Dirt
 		for (int i = 0; i < 30; i++)
 		{
+			if (tA < 1 && i % 8 == 0)
+			{
+				randL = Random.Range(5, 10);
+				randA = Random.Range(2, 4);
+				randP = Random.Range(0, 28 - randL);
+
+				tL = randL;
+				tA = randA;
+				Debug.Log(randA + " e " + randL);
+			} else if (tA > 0)
+			{
+				tL = randL;
+			}
+			tA--;
+
 			for (int j = 0; j < 28; j++)
 			{
-				Vector3 t = transform.position;
+				if (j < randP || tL == 0)
+				{
+					Vector3 t = transform.position;
 
-				t.x += j * (sr.bounds.size.x-0.003f);
-				t.y -= i * (sr.bounds.size.y-0.003f);
+					t.x += j * (sr.bounds.size.x-0.003f);
+					t.y -= i * (sr.bounds.size.y-0.003f);
 
-				GameObject obj = GameObject.Instantiate(dirt, t, Quaternion.identity) as GameObject;
+					GameObject obj = GameObject.Instantiate(dirt, t, Quaternion.identity) as GameObject;
 
-				obj.transform.parent = transform;
+					obj.transform.parent = transform;
+				} else if (j >= randP)
+				{
+					tL--;
+				}
+
+				if (tA == 1 && j == randP)
+				{
+					Vector3 t = transform.position;
+
+					t.x += j * (sr.bounds.size.x-0.003f);
+					t.y -= i * (sr.bounds.size.y-0.003f);
+
+					Espaço te = new Espaço(t, randA, randL);
+
+					e.Add(te);
+				}
 			}
 		}
-		///////// end Cobertura
+		///////// end Dirt
 	}
 }
